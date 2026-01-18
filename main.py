c13bc7bf11fbc8e6f88413db07e5ac231a6e6d37
kaiooprs <kaiopereira3.contato@gmail.com>
2026-01-18 17:07:10 -0300

    fixing syntax

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from database import init_db
 
+# Importando os módulos de rota
 from endpoints import user, account, category, transaction
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # Inicializa o banco ao subir a aplicação
     await init_db()
     print("Banco de Dados Conectado")
     yield
 
 app = FastAPI(lifespan=lifespan)
 
+# --- AQUI ESTAVA FALTANDO: Conectando as rotas ao app principal ---
 app.include_router(user.router, prefix="/users", tags=["Users"])
 app.include_router(account.router, prefix="/accounts", tags=["Accounts"])
 app.include_router(category.router, prefix="/categories", tags=["Categories"])
